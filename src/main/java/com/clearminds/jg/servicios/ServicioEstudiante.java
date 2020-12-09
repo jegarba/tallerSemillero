ba928defea2421add2d855785571edd36a816bad
jesus garcia <jegarba1996@gmail.com>
2020-12-09 12:12:53 -0500

    Test Actualizar

--- src/main/java/com/clearminds/jg/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/jg/servicios/ServicioEstudiante.java
@@ -7,27 +7,51 @@ import com.clearminds.jg.dtos.Estudiante;
 import com.clearminds.jg.excepciones.BDDException;
 
 public class ServicioEstudiante extends ServicioBase {
-	
-	public void insertarEstudiante(Estudiante estudiante) throws BDDException{
-		
+
+	public void insertarEstudiante(Estudiante estudiante) throws BDDException {
+
 		abrirConexion();
 		System.out.println("insertando estudiante: " + estudiante);
-		
+
 		Statement stmt = null;
-		
-		
+
 		try {
-			
-		stmt = getConexion().createStatement();
-		
-		String sql = "INSERT INTO estudiantes (nombre,apellido) VALUES ('" +estudiante.getNombre()+"','" +estudiante.getApellido()+"')";
-		System.out.println("Scrip: " + sql);
-		
-		stmt.executeUpdate(sql);
-		cerrarConexion();
+
+			stmt = getConexion().createStatement();
+
+			String sql = "INSERT INTO estudiantes (nombre,apellido) VALUES ('" + estudiante.getNombre() + "','"	+ estudiante.getApellido() + "')";
+			System.out.println("Scrip: " + sql);
+
+			stmt.executeUpdate(sql);
+			cerrarConexion();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new BDDException("error al insertar el estudiante");
 		}
 	}
+
+	public void actualizarEstudiante(Estudiante estudiante) throws BDDException {
+		
+		abrirConexion();
+		
+		System.out.println("Actualizando estudiante: " + estudiante);
+
+		Statement stmt = null;
+
+		try {
+
+			stmt = getConexion().createStatement();
+
+			String sql = "UPDATE estudiantes SET nombre= '"+estudiante.getNombre()+"', apellido='"+estudiante.getApellido()+"' where id=2";
+			System.out.println("Scrip: " + sql);
+
+			stmt.executeUpdate(sql);
+			cerrarConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("error al Actualizar el estudiante");
+		}
+		
+	}
+
 }
---
+++ src/main/java/com/clearminds/jg/test/TestActualizar.java
@@ -0,0 +1,19 @@
+package com.clearminds.jg.test;
+
+import com.clearminds.jg.dtos.Estudiante;
+import com.clearminds.jg.excepciones.BDDException;
+import com.clearminds.jg.servicios.ServicioEstudiante;
+
+public class TestActualizar {
+
+	public static void main(String[] args) {
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.actualizarEstudiante(new Estudiante("maria", "salas"));
+		} catch (BDDException e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+
+	}
+}
